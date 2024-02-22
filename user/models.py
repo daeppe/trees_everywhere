ea9308cc99d949450ce40674b00d1bbda1788f5f
Epichin <d.pena@hotmail.com>
2024-02-22 11:52:32 -0300

    Feat: Add functions to plant trees in user.

--- user/models.py
+++ user/models.py
@@ -1,1 +1,14 @@
+# from django.db import models
+from django.contrib.auth.models import AbstractUser
+from typing import Tuple, List
+from decimal import Decimal
+from ..tree.models import Tree
+
+
 # Create your models here.
+class User(AbstractUser):
+    def plant_tree(self, tree: Tree, location: Tuple[Decimal, Decimal]):
+        pass
+
+    def plant_trees(self, plants: List[Tree, Tuple[Decimal, Decimal]]):
+        pass
