8edcf7567444a661f7613f3a8be813e3f7706825
Epichin <d.pena@hotmail.com>
2024-02-21 23:55:09 -0300

    Feat: Add tree model and migrations.

--- tree/models.py
+++ tree/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Tree(models.Model):
+    name = models.CharField(max_length=50)
+    scientific_name = models.CharField(max_length=50)
