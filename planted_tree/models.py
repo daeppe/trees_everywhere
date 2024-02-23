09008de503cae12e60b05450cac34d08cbde3819
Epichin <d.pena@hotmail.com>
2024-02-23 12:20:11 -0300

    Feat: Add planted tree and migrations.

--- planted_tree/models.py
+++ planted_tree/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from user.models import User
+from tree.models import Tree
+from account.models import Account
+
 
 # Create your models here.
+class PlantedTree(models.Model):
+    age = models.IntegerField()
+    planted_at = models.DateTimeField(timezone.now)
+    location = models.CharField(max_length=50)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tree = models.ForeignKey(Tree, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
